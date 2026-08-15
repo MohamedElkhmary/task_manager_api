dfd0b6d3bf08dffecf8fa5cd66da917768e48ae1
MohamedElkhmary <hmwalkhmary03@gmail.com>
2026-08-15 15:05:43 +0300

    Update code

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-class Task(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
-    completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.title
+class Task(models.Model):
+  user = models.ForeignKey(
+      User, on_delete=models.CASCADE, null=True, blank=True
+  )
+  title = models.CharField(max_length=255)
+  description = models.TextField(blank=True, null=True)
+  completed = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.title
